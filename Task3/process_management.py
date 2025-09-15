788a4643e98969083e2932d76c2bdf9fdf02e484
Nishtha Chauhan <160705830+nishthachauhan2708@users.noreply.github.com>
2025-09-15 22:07:38 +0530

    process_management.py

--- Task3/process_management.py
+++ Task3/process_management.py
@@ -1,9 +1,7 @@
 import os
 import time
-
 def zombie_process():
     pid = os.fork()
-
     if pid == 0:
         print(f"[Child] PID={os.getpid()}, Parent PID={os.getppid()} -> Exiting now.")
         os._exit(0)
@@ -11,10 +9,8 @@ def zombie_process():
         print(f"[Parent] PID={os.getpid()}, created child PID={pid}")
         print("[Parent] Not calling wait(), sleeping... Run 'ps -el | grep defunct' to see zombie.")
         time.sleep(30)  
-
 def orphan_process():
     pid = os.fork()
-
     if pid == 0:
         print(f"[Child] PID={os.getpid()}, Parent PID={os.getppid()} -> Sleeping...")
         time.sleep(20)
@@ -22,13 +18,11 @@ def orphan_process():
     else:
         print(f"[Parent] PID={os.getpid()}, created child PID={pid} -> Exiting immediately.")
         os._exit(0)
-
 if __name__ == "__main__":
     print("\n=== Task 3: Zombie & Orphan Processes ===")
     print("1. Zombie process demo")
     print("2. Orphan process demo")
     choice = input("Enter choice: ")
-
     if choice == "1":
         zombie_process()
     elif choice == "2":
